9a54bd4c8020fa73d0e3b1e16d9b1d16fcee75d2
julianmaster <julienm86230@hotmail.fr>
2017-03-08 01:29:46 +0100

    Continue migration to JavaFX

--- src/ui/AsciiTerminalFX.java
+++ src/ui/AsciiTerminalFX.java
@@ -8,11 +8,12 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.effect.Blend;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.effect.ColorInput;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -20,7 +21,6 @@ public class AsciiTerminalFX extends Application {
 	
 	private int width;
 	private int height;
-//	private
 	
 
 	@Override
@@ -31,11 +31,6 @@ public class AsciiTerminalFX extends Application {
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		
-		Canvas canvas = new Canvas( 512, 512 );
-	    root.getChildren().add( canvas );
-	 
-	    GraphicsContext gc = canvas.getGraphicsContext2D();
-//	 
 //	    Image earth = new Image(new FileInputStream("earth.png"));
 //	    Image sun   = new Image(new FileInputStream("sun.png"));
 //	    Image space = new Image(new FileInputStream("space.png"));
@@ -59,24 +54,48 @@ public class AsciiTerminalFX extends Application {
 //	    }.start();
 	    
 	    
-	    Image i = new Image(new FileInputStream("src/assets/wanderlust_16x16.png"));
-		
-		Color c = i.getPixelReader().getColor(0, 0);
-		ColorInput topInput = new ColorInput(0, 0, 100, 100, c);
-
-		Blend blush = new Blend(
-                BlendMode.SCREEN,
-                null,
-                topInput
-        );
-		
-		gc.setEffect(blush);
-		
-		gc.drawImage(i, 0, 0);
+	    Image image = new Image(new FileInputStream("src/assets/wanderlust_16x16.png"));
 	    
+	    ImageView modified = new ImageView(createImage(image));
+	    
+	    root.getChildren().add(modified);
+	    
+//	    Color color = Color.BLUE;
+//	    double hue = map((color.getHue() + 180) % 360, 0, 360, -1, 1);
+//	    colorAdjust.setHue(hue);
+//	    double saturation = color.getSaturation();
+//	    colorAdjust.setSaturation(saturation);
+//	    double brightness = map(color.getBrightness(), 0, 1, -1, 0);
+//	    colorAdjust.setBrightness(brightness);
 	    
 	    stage.show();
 	}
+	
+	public static Image createImage(Image image) {
+		PixelReader pixelReader = image.getPixelReader();
+		
+		Color characterBackgroundColor = pixelReader.getColor(0, 0);
+		
+		WritableImage writableImage = new WritableImage((int)image.getWidth(), (int)image.getHeight());
+		PixelWriter pixelWriter = writableImage.getPixelWriter();
+		
+		for(int x = 0; x < image.getWidth(); x++) {
+			for(int y = 0; y < image.getHeight(); y++) {
+				if(pixelReader.getColor(x, y).equals(characterBackgroundColor)) {
+					pixelWriter.setColor(x, y, new Color(0, 0, 0, characterBackgroundColor.getOpacity()));
+				}
+				else {
+					pixelWriter.setColor(x, y, pixelReader.getColor(x, y));
+				}
+			}
+		}
+		
+		return writableImage;
+	}
+	
+	public static double map(double value, double start, double stop, double targetStart, double targetStop) {
+        return targetStart + (targetStop - targetStart) * ((value - start) / (stop - start));
+   }
 
 	public static void main(String[] args) {
 		AsciiTerminalFX.launch(args);
